7388996afd31a3c14daa05428bc9a60a8a8e2074
Hemanth <hemanthsai10@gmail.com>
2017-06-20 21:21:41 +0530

    Image converter_image reading

--- Image_conv/ImgConv.java
+++ Image_conv/ImgConv.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.io.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class ImgConv{
 	public static void main(String args[]) throws Exception{
@@ -6,5 +10,13 @@ public class ImgConv{
 		Scanner s= new Scanner(System.in);
 		String img_name=s.next();
 		System.out.println("File name is :"+img_name);
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File(img_name));
+		    System.out.println("done");
+		} catch (IOException ioe) {
+			System.out.println("File not found!!");
+			ioe.printStackTrace();
+		}
 	}
 }
