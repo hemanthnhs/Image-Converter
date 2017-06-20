ef85de7154a200a772fcaa8f7c72c2d30bceb09a
Hemanth <hemanthsai10@gmail.com>
2017-06-21 00:40:06 +0530

    Image converter grayscale done

--- Image_conv/ImgConv.java
+++ Image_conv/ImgConv.java
@@ -11,6 +11,7 @@ import javax.swing.JTextField;
 import java.awt.event.*;  
 
 public class ImgConv{
+	static BufferedImage img = null;
 	public BufferedImage grayscale_fn(BufferedImage img,int width,int height){
 		for(int i=0; i<height; i++){
             for(int j=0; j<width; j++){
@@ -29,7 +30,7 @@ public class ImgConv{
 		Scanner s= new Scanner(System.in);
 		String img_name=s.next();
 		System.out.println("File name is :"+img_name);
-		BufferedImage img = null;
+		
 		try {
 		    img = ImageIO.read(new File(img_name));
 		    System.out.println("done");
@@ -39,25 +40,39 @@ public class ImgConv{
 		}
 		int width= img.getWidth();
         int height = img.getHeight();
-        img= new ImgConv().grayscale_fn(img,width,height);
-		ImageIcon icon=new ImageIcon(img);
+        
+		
 		JFrame frame=new JFrame();
         frame.setLayout(new FlowLayout());
         frame.setSize(height,width);
         JButton b=new JButton("Grayscale");
         JButton c=new JButton("Black and White");
         b.setBounds(50,100,95,30);
-        b.addActionListener(new ActionListener(){  
-			public void actionPerformed(ActionEvent e){  
-          				System.out.println("yoo");
-			        }
-			    });  
+        ImageIcon icon=new ImageIcon(img);
         frame.add(b);  
         frame.add(c);
         JLabel lbl=new JLabel();
         lbl.setIcon(icon);
         frame.add(lbl);
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        b.addActionListener(new ActionListener(){  
+			public void actionPerformed(ActionEvent e){  
+						frame.getContentPane().removeAll();
+          				System.out.println("yoo");
+          				img= new ImgConv().grayscale_fn(img,width,height);
+          				System.out.println("yoo2");
+          				frame.remove(lbl);
+          				frame.add(b);  
+        				frame.add(c);
+				        JLabel lbl=new JLabel();
+				        lbl.setIcon(icon);
+				        frame.add(lbl);
+				        frame.setVisible(true);
+        				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			        }
+			    });
+        
+        
+        
 	}
 }
