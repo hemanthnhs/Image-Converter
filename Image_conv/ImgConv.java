1f9d004382fb481139f74cc5d8918f49eca46340
Hemanth <hemanthsai10@gmail.com>
2017-06-20 21:53:44 +0530

    Image converter img_grayscale

--- Image_conv/ImgConv.java
+++ Image_conv/ImgConv.java
@@ -21,10 +21,23 @@ public class ImgConv{
 			System.out.println("File not found!!");
 			ioe.printStackTrace();
 		}
+		int width = img.getWidth();
+        int height = img.getHeight();
+        for(int i=0; i<height; i++){
+            for(int j=0; j<width; j++){
+               Color c = new Color(img.getRGB(j, i));
+               int red = (int)(c.getRed() * 0.299);
+               int green = (int)(c.getGreen() * 0.587);
+               int blue = (int)(c.getBlue() *0.114);
+               Color newColor = new Color(red+green+blue,red+green+blue,red+green+blue);
+               img.setRGB(j,i,newColor.getRGB());
+            }
+         }
+
 		ImageIcon icon=new ImageIcon(img);
 		JFrame frame=new JFrame();
         frame.setLayout(new FlowLayout());
-        frame.setSize(img.getHeight(),img.getWidth());
+        frame.setSize(height,width);
         JLabel lbl=new JLabel();
         lbl.setIcon(icon);
         frame.add(lbl);
