dc4c96e6ceb8cdea526cde85993cb3c930902e15
Hemanth <hemanthsai10@gmail.com>
2017-06-21 13:20:46 +0530

    Image converter_sepia tone filter

--- Image_conv/ImgConv.java
+++ Image_conv/ImgConv.java
@@ -25,6 +25,63 @@ public class ImgConv{
          }
          return img;
 	}
+	public BufferedImage bwscale_fn(BufferedImage img,int width,int height){
+		for(int i=0; i<height; i++){
+            for(int j=0; j<width; j++){
+               Color c = new Color(img.getRGB(j, i));
+               int val= ((int)c.getRed() +(int)c.getRed() +(int)c.getRed())/3; 
+               //System.out.println("Colors are"+val);
+               int red = val;
+               int green = val;
+               int blue = val;
+               Color newColor = new Color(val,val,val);
+               img.setRGB(j,i,newColor.getRGB());
+            }
+         }
+         return img;
+	}
+	public BufferedImage sepiaScale_fn(BufferedImage img,int width,int height){
+		for(int i=0; i<height; i++){
+            for(int j=0; j<width; j++){
+            	int sepiaDepth = 20;
+              int rgb = img.getRGB(j, i);
+	            Color color = new Color(rgb, true);
+	            int r = color.getRed();
+	            int g = color.getGreen();
+	            int b = color.getBlue();
+	            int gry = (r + g + b) / 3;
+
+	            r = g = b = gry;
+	            r = r + (sepiaDepth * 2);
+	            g = g + sepiaDepth;
+
+	            if (r > 255) {
+	                r = 255;
+	            }
+	            if (g > 255) {
+	                g = 255;
+	            }
+	            if (b > 255) {
+	                b = 255;
+	            }
+
+	            // Darken blue color to increase sepia effect
+	            //b -= sepiaIntensity;
+
+	            // normalize if out of bounds
+	            if (b < 0) {
+	                b = 0;
+	            }
+	            if (b > 255) {
+	                b = 255;
+	            }
+
+	            color = new Color(r, g, b, color.getAlpha());
+	            img.setRGB(j, i, color.getRGB());
+            }
+         }
+         return img;
+	}
 	public static void main(String args[]) throws Exception{
 		System.out.println("Enter File name :");
 		Scanner s= new Scanner(System.in);
@@ -47,10 +104,14 @@ public class ImgConv{
         frame.setSize(height,width);
         JButton b=new JButton("Grayscale");
         JButton c=new JButton("Black and White");
+        JButton d=new JButton("Sepia Tone");
+        JButton e=new JButton("Blur");
         b.setBounds(50,100,95,30);
         ImageIcon icon=new ImageIcon(img);
         frame.add(b);  
         frame.add(c);
+        frame.add(d);
+        
         JLabel lbl=new JLabel();
         lbl.setIcon(icon);
         frame.add(lbl);
@@ -64,6 +125,45 @@ public class ImgConv{
           				frame.remove(lbl);
           				frame.add(b);  
         				frame.add(c);
+        				frame.add(d);
+        				
+				        JLabel lbl=new JLabel();
+				        lbl.setIcon(icon);
+				        frame.add(lbl);
+				        frame.setVisible(true);
+        				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			        }
+			    });
+        
+        c.addActionListener(new ActionListener(){  
+			public void actionPerformed(ActionEvent e){  
+						frame.getContentPane().removeAll();
+          				System.out.println("yoo");
+          				img= new ImgConv().bwscale_fn(img,width,height);
+          				System.out.println("yoo2");
+          				frame.remove(lbl);
+          				frame.add(b);  
+        				frame.add(c);
+        				frame.add(d);
+        				
+				        JLabel lbl=new JLabel();
+				        lbl.setIcon(icon);
+				        frame.add(lbl);
+				        frame.setVisible(true);
+        				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			        }
+			    });
+         d.addActionListener(new ActionListener(){  
+			public void actionPerformed(ActionEvent e){  
+						frame.getContentPane().removeAll();
+          				System.out.println("yoo");
+          				img= new ImgConv().sepiaScale_fn(img,width,height);
+          				System.out.println("yoo2");
+          				frame.remove(lbl);
+          				frame.add(b);  
+        				frame.add(c);
+        				frame.add(d);
+        				
 				        JLabel lbl=new JLabel();
 				        lbl.setIcon(icon);
 				        frame.add(lbl);
